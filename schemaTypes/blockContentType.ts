87005d5afdeacab766c488e552cd87f89aa2bd7b
nihatonal <onalnihat1986@gmail.com>
2026-05-07 16:04:47 +0300

    added code

--- schemaTypes/blockContentType.ts
+++ schemaTypes/blockContentType.ts
@@ -21,6 +21,7 @@ export const blockContentType = defineType({
         decorators: [
           { title: "Strong", value: "strong" },
           { title: "Emphasis", value: "em" },
+          { title: "Code", value: "code" }, 
         ],
         annotations: [
           {
